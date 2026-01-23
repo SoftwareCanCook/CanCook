08862e33fcbf3497e13b49f14e92236f3894da8b
SushiBoiNV <nathanvu1111@gmail.com>
2026-01-22 23:49:44 -0500

    added springboot to api so that it will be able to run, which it does (I hope)

---
+++ src/main/java/com/example/demo/CancookApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CancookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CancookApplication.class, args);
+	}
+
+}
